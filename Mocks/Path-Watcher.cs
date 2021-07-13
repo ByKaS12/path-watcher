6a0bf1a915d2390a364909bde5048c1a26a890e8
ByKaS12 <67284495+ByKaS12@users.noreply.github.com>
2021-07-13 10:47:35 +0300

    Add class watchers and test function for it

---
+++ Mocks/Path-Watcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace path_watcher.Mocks
+{
+    public class Path_Watcher
+    {
+        private List<FileSystemWatcher> Watchers;
+        public Path_Watcher()
+        {
+            Watchers = new List<FileSystemWatcher>();
+        }
+        public void AddWatcher(string FullPath) => Watchers.Add(new FileSystemWatcher(FullPath));
+        public void DeleteWatcher(string FullPath) => Watchers.Remove(new FileSystemWatcher(FullPath));
+        public FileSystemWatcher GetWatcher(string FullPath) => Watchers.FirstOrDefault(x => x.Path == FullPath);
+        public List<FileSystemWatcher> GetAllWatchers() => Watchers;
+    }
+}
