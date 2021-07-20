5c31dea8b4ef54ef389aaed392e237047a16be51
ByKaS12 <67284495+ByKaS12@users.noreply.github.com>
2021-07-20 17:33:48 +0300

    Add tray and autorun program

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Collections.Generic;
 using path_watcher.Pages;
 using path_watcher.Static;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace path_watcher
 {
@@ -20,18 +21,86 @@ namespace path_watcher
     public partial class MainWindow : Window
     {
         private Dictionary<string, Type> pages = new Dictionary<string, Type>();
-
+        TaskbarIcon tbi;
+        private ApplicationContext Context;
+        private BaseRepository db;
+       
         public MainWindow()
         {
             InitializeComponent();
 
+            Context = new ApplicationContext();
+            db = new BaseRepository(Context);
+            tbi = (TaskbarIcon)FindResource("NotifyIcon");
+            tbi.Icon = new System.Drawing.Icon(@"C:\Users\SEEGa\source\repos\theDimZone\path-watcher\Icons\eye.ico");
+            tbi.TrayMouseDoubleClick += Tbi_TrayMouseDoubleClick;
+            tbi.TrayRightMouseDown += Tbi_TrayRightMouseDown;
+            tbi.TrayToolTipOpen += Tbi_TrayToolTipOpen;
+            this.Deactivated += MainWindow_Deactivated;
             SuperVisor.MountWatchers(Config.GetStringArray("paths"));
-
             pages.Add("FilesPage", typeof(FilesPage));
             pages.Add("SettingsPage", typeof(SettingsPage));
             pages.Add("LogsPage", typeof(LogsPage));
 
             nv.SelectedItem = nv.MenuItems.OfType<ModernWpf.Controls.NavigationViewItem>().First();
+        }
+
+        private void Tbi_TrayToolTipOpen(object sender, RoutedEventArgs e)
+        {
+            var list = db.GetLogs();
+            list.Sort((a, b) => b.DateEvent.CompareTo(a.DateEvent));
+            List<Log> SortList;
+            if (list.Count > 5) SortList = list.GetRange(0, 5); else SortList = list.GetRange(0, list.Count);
+            var listView = tbi.TrayToolTip as ListView;
+            listView.ItemsSource = SortList;
+            listView.Background = System.Windows.Media.Brushes.White;
+            
+        }
+
+        private void Tbi_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+
+                tbi.TrayToolTip.Visibility = Visibility.Visible;
+
+            }
+        }
+
+        private void Tbi_TrayRightMouseDown(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == WindowState.Minimized)
+            {
+
+                tbi.ContextMenu = new ContextMenu();
+                
+            }
+
+
+            
+
+        }
+
+        private void Tbi_TrayMouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this.WindowState = WindowState.Normal;
+                this.ShowInTaskbar = true;
+                tbi.Visibility = Visibility.Hidden;
+                this.Activate();
+
+            }
+        }
+
+        private void MainWindow_Deactivated(object sender, EventArgs e)
+        {
+            if(this.WindowState == WindowState.Minimized)
+            {
+
+                this.ShowInTaskbar = false;
+                tbi.Visibility = Visibility.Visible;
+            }
         }
 
         private void NavigationView_SelectionChanged(ModernWpf.Controls.NavigationView sender, ModernWpf.Controls.NavigationViewSelectionChangedEventArgs args)
--- Pages/SettingsPage.xaml.cs
+++ Pages/SettingsPage.xaml.cs
@@ -19,7 +19,8 @@ using path_watcher.Static;
 using System.IO;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-
+using Microsoft.Win32;
+using System.Reflection;
 
 namespace path_watcher.Pages
 {
@@ -32,15 +33,45 @@ namespace path_watcher.Pages
         private BaseRepository db;
         private ObservableCollection<string> paths;
         private ObservableCollection<string> filters; // TODO: add inputs for filters
-
+        private static bool SetAutoRunValue(bool autorun, string path)
+        {
+            const string name = "Path-Watcher";
+            string ExePath = path;
+            RegistryKey reg;
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                if (autorun)
+                {
+                    reg.SetValue(name, ExePath);
+                }
+                else
+                {
+                    reg.DeleteValue(name);
+                }
+                reg.Flush();
+                reg.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
         public SettingsPage()
         {
             InitializeComponent();
             Context = new ApplicationContext();
             db = new BaseRepository(Context);
-
+            IsAutoRun.Click += IsAutoRun_Click;
             paths = new ObservableCollection<string>(Config.GetStringArray("paths"));
             watchersList.ItemsSource = paths;
+        }
+
+        private void IsAutoRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsAutoRun.IsChecked == true) SetAutoRunValue(true, Assembly.GetExecutingAssembly().Location);
+            else SetAutoRunValue(false, Assembly.GetExecutingAssembly().Location);
         }
 
         private void AddWatcher_Click(object sender, RoutedEventArgs e)
@@ -83,6 +114,11 @@ namespace path_watcher.Pages
         private void ExportExcel_Click(object sender, RoutedEventArgs e)
         {
             db.ExportToExcel();
+
+        }
+
+        private void IsAutoRun_Checked(object sender, RoutedEventArgs e)
+        {
 
         }
     }
